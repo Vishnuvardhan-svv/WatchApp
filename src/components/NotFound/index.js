42c01431ce83d5da9cc37fce9ddc5c16a140575d
Vishnuvardhan-svv <vishnuvardhan.svv123@gmail.com>
2026-05-11 07:19:21 +0000

    Added NotFound Component

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,29 @@
+import AppTheme from '../../context/AppTheme'
+import {DivElememt, ImageElement} from './styledComponents'
+
+const NotFound = () => (
+  <AppTheme.Consumer>
+    {value => {
+      const {activeTheme} = value
+      const bgColor = activeTheme === light ? '#ffffff' : '#000000'
+      const color = activeTheme === light ? '#000000' : '#ffffff'
+      return (
+        <DivElememt bgColor={bgColor} color={color}>
+          {activeTheme === 'light' ? (
+            <>
+              <ImageElement src="https://assets.ccbp.in/frontend/react-js/nxt-watch-not-found-light-theme-img.png" />
+            </>
+          ) : (
+            <>
+              <ImageElement src="https://assets.ccbp.in/frontend/react-js/nxt-watch-not-found-dark-theme-img.png" />
+            </>
+          )}
+          <h1>Page Not Found</h1>
+          <p>We are sorry,the page you requested could not be found.</p>
+        </DivElememt>
+      )
+    }}
+  </AppTheme.Consumer>
+)
+
+export default NotFound
