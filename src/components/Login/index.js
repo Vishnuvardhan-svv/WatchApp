1c6b23f592c1fd0dc9c821230389b2b2a995752e
Vishnuvardhan-svv <vishnuvardhan.svv123@gmail.com>
2026-05-11 12:52:06 +0000

    Added Loader and Home Page

---
+++ src/components/Login/index.js
@@ -0,0 +1,131 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+
+import {Redirect} from 'react-router-dom'
+
+import {
+  LoginContainer,
+  ShadowContainer,
+  LoginDivContainer,
+  ImageEl,
+  InputEl,
+  LabelEl,
+  ButtonEl,
+  ErrorMsg,
+} from './styledComponents'
+
+class Login extends Component {
+  state = {
+    visibility: false,
+    username: '',
+    password: '',
+    showSubmitError: false,
+    errorMsg: '',
+  }
+
+  showPassword = event => {
+    if (event.target.checked) {
+      this.setState({visibility: true})
+    } else {
+      this.setState({visibility: false})
+    }
+  }
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmitSuccess = jwtToken => {
+    const {history} = this.props
+    Cookies.set('jwt_token', jwtToken, {
+      expires: 30,
+      path: '/',
+    })
+
+    history.replace('/')
+  }
+
+  onSubmitFailure = errorMsg => {
+    this.setState({showSubmitError: true, errorMsg})
+  }
+
+  formSubmit = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    if (response.ok === true) {
+      this.onSubmitSuccess(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+  }
+
+  render() {
+    const {visibility, username, password, showSubmitError, errorMsg} =
+      this.state
+
+    const jwtToken = Cookies.get('jwt_token')
+    if (jwtToken !== undefined) {
+      return <Redirect to="/" />
+    }
+
+    return (
+      <LoginContainer>
+        <ShadowContainer>
+          <ImageEl
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+            alt="website logo"
+          />
+          <form onSubmit={this.formSubmit}>
+            <LoginDivContainer>
+              <LabelEl htmlFor="username">USERNAME</LabelEl>
+              <InputEl
+                id="username"
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={this.onChangeUsername}
+              />
+            </LoginDivContainer>
+            <LoginDivContainer>
+              <LabelEl htmlFor="password">PASSWORD</LabelEl>
+              <InputEl
+                id="password"
+                type={visibility ? 'text' : 'password'}
+                placeholder="Password"
+                value={password}
+                onChange={this.onChangePassword}
+              />
+            </LoginDivContainer>
+            <LoginDivContainer direction="row">
+              <InputEl
+                type="checkbox"
+                id="checkbox"
+                onChange={this.showPassword}
+              />
+              <LabelEl htmlFor="checkbox" cursor="pointer">
+                Show Password
+              </LabelEl>
+            </LoginDivContainer>
+            <ButtonEl type="submit">Login</ButtonEl>
+            {showSubmitError && (
+              <ErrorMsg className="error-message">*{errorMsg}</ErrorMsg>
+            )}
+          </form>
+        </ShadowContainer>
+      </LoginContainer>
+    )
+  }
+}
+
+export default Login
